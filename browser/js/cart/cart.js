492e5418fc9b6874dfabb6979beeb942676f1746
thinkocapo <thinkocapo@gmail.com>
2015-11-02 16:39:31 -0500

    added cartController

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -1,10 +1,15 @@
-app.config(function($stateProvider){
+app.config(function($stateProvider, CartFactory){
 	$stateProvider.state("cart",{
 		url:"/cart",
 		templateUrl:"js/cart/cart.html",
 		controller:"cartCtrl",
 		resolve: {
-			
+			retrievedOrder : function(){
+				return CartFactory.getCurrentOrder()
+				.then(function(retrievedOrder){
+					return retrievedOrder
+				})
+			}
 		}
 	})
 })
