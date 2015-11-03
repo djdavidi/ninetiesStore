b54ece3524a9c86d6876a33989a70374158c8ba5
[David Scherban] <[davidscherban@gmail.com]>
2015-11-03 12:25:51 -0500

    setting up order-test

---
+++ tests/server/models/order-test.js
@@ -0,0 +1,74 @@
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB = require('mocha-mongoose')(dbURI);
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+
+// Require in all models.
+require('../../../server/db/models');
+
+var User = mongoose.model('User');
+var Product = mongoose.model('Product');
+
+describe("Order model",function(){
+	 var createUser = function () {
+          return User.create({ email: 'obama@gmail.com', password: 'potus', address: "Wash the DC"});
+      };
+      var createOrder = function () {
+          return Order.create(owner: createUser());
+      };
+
+	beforeEach("Establish DB connection",function(done){
+		if (mongoose.connection.db) return done();
+        mongoose.connect(dbURI, done);
+	})
+	  afterEach('Clear test database', function (done) {
+        clearDB(done);
+    });
+
+    it('should exist', function () {
+        expect(Product).to.be.a('function');
+    });
+
+    describe("pre save hook for User's email and address fills in correctly",
+    	function(){
+
+    		it("should have the User's valid email",function(done){
+    			createOrder()
+    			.then(function(newOrder){
+    				expect(newOrder.email).to.be.equal("obama@gmail.com");
+    				expect(newOrder.address).to.be.equal("Wash the DC");
+    				done();
+    			})
+    		})
+    	})
+
+    describe("addItem method on the Order instance",function(){
+    	it("should find the correct item")
+
+
+    	it("should call update quantity with correct amount")
+
+    	it("should add the item properly")
+
+
+
+
+
+
+    })
+
+  
+
+
+
+
+
+
+
+
+
+
+
+})
