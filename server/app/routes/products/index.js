0f434b2aca4e609e3ed8696b22c7f81524ead65f
Josh Mazen <joshmazen14@gmail.com>
2015-11-03 10:58:19 -0500

    Product routes

---
+++ server/app/routes/products/index.js
@@ -0,0 +1,51 @@
+var router = require('express').Router()
+var Product = mongoose.model('Product')
+
+router.get('/', function(req, res, next) {
+	Product.find().exec()
+	.then(function(products) {
+		res.send(products)
+	})
+	.then(null, next)
+})
+
+router.get('/:id', function(req, res, next) {
+	Product.findById(req.params.id)
+	.then(function(product) {
+		res.send(product)
+	})
+	.then(null, next)
+})
+
+router.post('/', function(req, res, next) {
+	Product.create(req.body)
+	.then(function(product) {
+		res.status(201).send(product)
+	})
+	.then(null, next)
+})
+
+router.put('/:id', function(req, res, next) {
+	Product.findById(req.params.id)
+	.then(function(product) {
+		for (var key in req.body) {
+			product[key] = req.body[key]
+		}
+		return product.save()
+	})
+	.then(function(savedProduct) {
+		res.send(savedProduct)
+	})
+	.then(null, next)
+})
+
+router.delete('/:id', function(req, res, next) {
+	Product.findById(req.params.id)
+	.then(function(product) {
+		product.remove()
+		res.status(204)
+	})
+	.then(null, next)
+})
+
+module.exports = router;
