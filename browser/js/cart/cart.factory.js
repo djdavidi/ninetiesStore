9ef45c31e117bc56e078692a781a23bfdab62a10
thinkocapo <thinkocapo@gmail.com>
2015-11-05 18:00:34 -0500

    can add and update quantity. started Cache for realtime removal for cart

--- browser/js/cart/cart.factory.js
+++ browser/js/cart/cart.factory.js
@@ -1,21 +1,28 @@
 app.factory("CartFactory",function($http){
+	var cachedCart = [];
 	return {
+			getCachedCart: function(){
+				return cachedCart
+			},
 			getCurrentOrder:function(){
 				// if(!currentUser) currentUser = "";
 				return $http.get("/api/cart/")
 				.then(function(response){
-					return response.data;
+					angular.copy(response.data, cachedCart);
+					return cachedCart;
 				})
 			},
 			add:function(itemId, quantity){
 				return $http.put("/api/cart/" + itemId,{quantity:quantity})
 				.then(function(response){
-					console.log("response is:", response)
+					console.log("storedItems[0]", response.data.storedItems[0])
+					console.log("cachedCart", cachedCart)
+					cachedCart.push(response.data.storedItems[response.data.storedItems.length-1]);
 					return response.data;
 				})
 			},
 			removeItem:function(itemId){
-				return $http.delete("/api/cart/:itemId")
+				return $http.delete("/api/cart/" + itemId)
 			}
 			// need some way of updating change and when the change is to the 
 			// cart itself making it a past order we need to change cart and create a new order..
