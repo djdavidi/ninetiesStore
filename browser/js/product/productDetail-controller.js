ba544aa23c42a45d658e3b7416b1e22895adf4d3
Luis Martins <luis.m.martins91@gmail.com>
2015-11-05 12:03:24 -0500

    we fixed the things we wanted to fix

--- browser/js/product/productDetail-controller.js
+++ browser/js/product/productDetail-controller.js
@@ -2,6 +2,6 @@ app.controller('ProductDetailCtrl', function($scope, productDetail, productRevie
 	$scope.ProductDetail = productDetail;
 	$scope.ProductReviews = productReviews
 	$scope.add = function(id, currentUser){
-		return CartFactory.addItem(id)
+		return CartFactory.add(id)
 	}
 })
