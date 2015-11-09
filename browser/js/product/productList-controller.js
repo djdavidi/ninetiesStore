8042ee7b8827d87850dedf88de0fec341b6a1951
Luis Martins <luis.m.martins91@gmail.com>
2015-11-09 18:18:22 -0500

    addToCart button works now

--- browser/js/product/productList-controller.js
+++ browser/js/product/productList-controller.js
@@ -1,4 +1,10 @@
-app.controller('ProductListCtrl', function($scope, products, categories){
+app.controller('ProductListCtrl', function($scope, products, categories, CartFactory, $state){
 	$scope.products = products;
 	$scope.categories = categories;
+	$scope.add = function(id, currentUser){
+		return CartFactory.add(id, 1)
+		.then(function() {
+			$state.go('cart')
+		})
+	}
 })
