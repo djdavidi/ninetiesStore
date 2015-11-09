cf206103cab72baea1cb422a87c3ddee4480a63a
[David Scherban] <[davidscherban@gmail.com]>
2015-11-09 15:04:36 -0500

    laying out structure

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -1,3 +1,40 @@
-app.controller('AdminCtrl', function ($scope) {
+app.controller('AdminCtrl', function ($scope,AuthService) {
+			$scope.getAnyUser=function(name){
+
+			}
+			$scope.updateAnyUser=function(id,change){
+
+			}
+			$scope.deleteAnyUser=function(id){
+
+			}
+			//Create and edit products with name, description,
+			//price and one or more photos
+			$scope.createProduct=function(){
+
+			}
+			$scope.editProduct=function(){
+
+			}
+			$scope.getAllOrders=function(){
+
+			}
+			$scope.changeOrderStatus=function(){
+
+			}
+
+			$scope.getOneOrder=function(){
+
+			}
+			$scope.passwordReset=function(userId){
+				//have to send that over to user to prompt them
+				//on first time they log in again
+			}
+
+			$scope.promoCodes=function(){
+
+			}
+
+
 
 });
--- browser/js/admin/admin.factory.js
+++ browser/js/admin/admin.factory.js
@@ -1,3 +1,23 @@
 app.factory("AdminFactory",function($http){
-	
+	return {
+			getAnyUser:function(name){
+
+			},
+			updateAnyUser:function(id,change){
+
+			},
+			deleteAnyUser:function(id){
+
+			}
+
+
+
+
+
+
+
+
+
+
+	}
 })
