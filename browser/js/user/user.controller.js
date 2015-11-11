bce6d5bb6529ac4cbf7f9f6dd830712350376535
Josh Mazen <joshmazen14@gmail.com>
2015-11-10 23:06:45 -0500

    Updated user page

--- browser/js/user/user.controller.js
+++ browser/js/user/user.controller.js
@@ -1,7 +1,9 @@
 app.controller('UserCtrl', function($scope, user, reviews, orders, ProductFactory) {
 	$scope.user = user;
 	$scope.reviews = reviews
-	$scope.orders = orders
+	$scope.orders = orders.filter(function(order) {
+		return order.status !== 'Created'
+	})
 	$scope.convertDate = function(date) {
 		var dateStr = date.slice(0, 10)
 		return dateStr.split('-').reverse().join('/')
